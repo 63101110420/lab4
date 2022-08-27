ef82d5d884b625b0b7280ac97cdf6574c930f3a9
Laila <lailar3513@gmail.com>
2022-08-27 22:31:55 +0700

    part1

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   }
   return (
   <View style={styles.container}>
-  <Text onPress={doIt}>Hello world</Text>
+  <Text onPress={doIt}>Hello </Text>
   <StatusBar style="auto" />
   </View>
   );
